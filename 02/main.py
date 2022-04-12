b2b54a7a01149b80ab6a61d8caa947a8fa795045
Mary Kuznetsova <masha.kuznet@gmail.com>
2022-04-12 17:12:45 +0300

    dz2_commit2

--- 02/main.py
+++ 02/main.py
@@ -1,14 +1,17 @@
 
 def parse_html(html_str, open_tag_callback, data_callback, close_tag_callback):
     i = 0
+    # Список для открытых тегов
     list_teg = []
     while html_str.find('<', i) != -1:
         i = html_str.find('<', i)
+        # Обработка открывающего тега
         if html_str[i+1] != '/':
             j = html_str.find('>', i)
             open_tag_callback(html_str[i:j+1])
             list_teg.append([html_str[i:j+1], j+1])
             i = j
+        # Обработка закрывающего тега и текста между тегами
         else:
             j = html_str.find('>', i)
             teg_now = '<' + html_str[i+2:j+1]
@@ -19,10 +22,13 @@ def parse_html(html_str, open_tag_callback, data_callback, close_tag_callback):
                     break
             close_tag_callback(html_str[i:j+1])
             i = j
+    # Тут я вызываю функцию обработчик текста в случае, если так и не был встречен закрывающий тег
+    # Возможно этого не надо было делать, я просто думвлв так учесть, что бывают только открывающие теги
     if list_teg:
         for teg in list_teg:
             data_callback(html_str[teg[1]:])
 
 
+# Функция для тестирования через mock
 def foo(s):
     print(s)
--- 02/tests.py
+++ 02/tests.py
@@ -6,12 +6,15 @@ from main import parse_html
 
 class TestParseHtml(unittest.TestCase):
 
+    # Тестируем вызов функции, обрабатывающей открывающий тег
     @patch('main.foo', return_value=None)
     def test_open_tag_callback(self, foo_mock):
+        # Провкряем, что если подходящих тегов нет, то ничего не вызывается
         parse_html('Текст', foo_mock, len, len)
         self.assertEqual(foo_mock.call_count, 0)
         foo_mock.assert_not_called()
 
+        # Проверяем разные цепочки вызовов
         parse_html('<p><i>Текст</i></p>', foo_mock, len, len)
         self.assertEqual(foo_mock.call_count, 2)
         foo_mock.assert_any_call('<p>')
@@ -25,8 +28,10 @@ class TestParseHtml(unittest.TestCase):
         parse_html(fake.bothify(text="<a>?? ????? <?> ? <?>? </a>"), foo_mock, len, len)
         self.assertEqual(foo_mock.call_count, 6)
 
+    # Тестируем вызов функции, обрабатывающей закрывающий тег
     @patch('main.foo', return_value=None)
     def test_close_tag_callback(self, foo_mock):
+        # Провкряем, что если подходящих тегов нет, то ничего не вызывается
         parse_html('Текст', len, len, foo_mock)
         self.assertEqual(foo_mock.call_count, 0)
         foo_mock.assert_not_called()
@@ -35,18 +40,22 @@ class TestParseHtml(unittest.TestCase):
         self.assertEqual(foo_mock.call_count, 0)
         foo_mock.assert_not_called()
 
+        # Проверяем разные цепочки вызовов
         parse_html('<m>Tекст1</m><p>Tекст_2<p>Tекст_3</p>', len, len, foo_mock)
         self.assertEqual(foo_mock.call_count, 2)
         foo_mock.assert_any_call('</p>')
         foo_mock.assert_any_call('</m>')
 
+    # Тестируем вызов функции, обрабатывающей внутренний текст
     @patch('main.foo', return_value=None)
     def test_data_callback(self, foo_mock):
+        # Провкряем, что если подходящих тегов нет, то ничего не вызывается
         fake = Faker()
         parse_html(fake.text(max_nb_chars=50), len, foo_mock, len)
         self.assertEqual(foo_mock.call_count, 0)
         foo_mock.assert_not_called()
 
+        # Проверяем разные цепочки вызовов
         parse_html('<m>Tекст', len, foo_mock, len)
         self.assertEqual(foo_mock.call_count, 1)
         foo_mock.assert_any_call('Tекст')
